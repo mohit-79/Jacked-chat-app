9ff29f03ca882167c23fd8a532363e8b80d64e27
mohit-79 <mohitharjani79@gmail.com>
2026-07-17 20:38:48 +0530

    Clean up repository: remove legacy backend, untrack   configs, node_modules, and docs

--- backend/test.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
-
-uri = "mongodb+srv://mohitharjani79_db_user:MwCbnIQXIPR9YUO0@cluster0.ympqrth.mongodb.net/?appName=Cluster0"
-
-client = MongoClient(uri, server_api=ServerApi("1"))
-
-print(client.admin.command("ping"))
